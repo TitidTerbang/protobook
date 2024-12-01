8c8abdfa42d247ee3766daa2f69ff71de8fbd76e
TitidTerbang <yudisbaek@gmail.com>
2024-12-02 02:59:21 +0700

    geolocator, maps untuk perpustakaan terdekat

--- lib/views/library_screen.dart
+++ lib/views/library_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LibraryScreen extends StatefulWidget {
   @override
@@ -11,6 +12,14 @@ class _LibraryScreenState extends State<LibraryScreen> {
   Position? currentPosition;
   String currentLocation = 'Memuat lokasi...';
   bool loading = false;
+  GoogleMapController? _mapController;
+  Set<Marker> _markers = {};
+
+  // Koordinat tetap untuk perpustakaan pemerintah
+  final _perpustakaanPemerintah = LatLng(-7.972218034860499, 112.62250594970973);
+  final _gramediaBasukiRahmat = LatLng(-7.98101952219401, 112.63026841544281);
+  final _gramediaMog = LatLng(-7.977127454448862, 112.62351333707267);
+  final _ummBookstore = LatLng(-7.920581709000431, 112.59421975859856);
 
   @override
   void initState() {
@@ -48,6 +57,11 @@ class _LibraryScreenState extends State<LibraryScreen> {
         currentLocation =
         "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
         loading = false;
+        _addMarker(LatLng(position.latitude, position.longitude), "Lokasi Anda");
+        _addMarker(_perpustakaanPemerintah, "Perpustakaan Pemerintah");
+        _addMarker(_gramediaBasukiRahmat, "Gramedia Basuki Rahmat Malang");
+        _addMarker(_gramediaMog, "Gramedia MOG Malang");
+        _addMarker(_ummBookstore, "UMM Bookstore");
         print(currentLocation);
       });
     } catch (e) {
@@ -56,6 +70,16 @@ class _LibraryScreenState extends State<LibraryScreen> {
         currentLocation = 'Gagal mendapatkan lokasi: ${e.toString()}';
       });
     }
+  }
+
+  void _addMarker(LatLng position, String title) {
+    _markers.add(
+      Marker(
+        markerId: MarkerId(title),
+        position: position,
+        infoWindow: InfoWindow(title: title),
+      ),
+    );
   }
 
   @override
@@ -74,15 +98,33 @@ class _LibraryScreenState extends State<LibraryScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              currentLocation,
-              style: GoogleFonts.montserrat(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
+            // KODE UNTUK CEK LOKASI USER ENJOY MY MEN
+            // Text(
+            //   currentLocation,
+            //   style: GoogleFonts.montserrat(fontSize: 16),
+            //   textAlign: TextAlign.center,
+            // ),
             Text(
               'Daftar Perpustakaan Terdekat akan ditampilkan di sini.',
               style: GoogleFonts.montserrat(fontSize: 16),
               textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 300,
+              child: currentPosition != null
+                  ? GoogleMap(
+                      onMapCreated: (controller) {
+                        _mapController = controller;
+                      },
+                      initialCameraPosition: CameraPosition(
+                        target: LatLng(currentPosition!.latitude, currentPosition!.longitude),
+                        zoom: 15.0,
+                      ),
+                      markers: _markers,
+                      myLocationEnabled: true,
+                      myLocationButtonEnabled: true,
+                    )
+                  : const Center(child: CircularProgressIndicator()),
             ),
           ],
         ),
